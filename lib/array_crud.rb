0b86a9038114daa4cd95aa5b2ac7011e6196586b
Natalie So <nataliesotszyui@gmail.com>
2020-02-12 21:59:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -24,7 +24,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  return array[index]
+  a =  array[index]
+  return a
 end
 
 def retrieve_first_element_from_array(array)
